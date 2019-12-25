91339c409a1e5af5e11ec90d43ac80fe88287f79
purerosefallen <78877@qq.com>
2019-12-26 07:15:25 +0800

    update format

--- coolq.js
+++ coolq.js
@@ -11,7 +11,7 @@ this.init = (client, options) => {
 		client.bot("send_msg", send_data);
 	}
 	client.send_help = (data) => { 
-		client.reply(data, "输入\n/create [人头赛|KOF]\nA队名 A队员1 A队员2 A队员3 ...\nB队名 B队员1 B队员2 B队员3 ...\n即可创建比赛。");
+		client.reply(data, "输入\n/create [人头赛|KOF]\nA队名：A队员1 A队员2 A队员3 ...\nB队名：B队员1 B队员2 B队员3 ...\n即可创建比赛。");
 	}
 	client.bot.on("message", (data) => {
 		const msg = data.message.trim();
@@ -31,29 +31,23 @@ this.init = (client, options) => {
 				const is_kof = (!parsed_msg[1] || !parsed_msg[1].startsWith("人头"));
 				const place = data.group_id;
 				const teams = [];
-				const line_txts = [];
 				for (var i = 0; i < 2; ++i) { 
-					const line_txt = lines[i + 1];
-					const temp = line_txt.split(" ");
-					if (temp.length < 2) { 
+					const line_txt = lines[i + 1].trim();
+					const temp1 = line_txt.match(/^(.+?)[:\uff1a](.+)$/);
+					if (!temp1) { 
 						client.send_help();
 						return;
 					}
-					line_txts.push(temp);
+					const team_name = temp1[1].trim();
+					const team_player_names = temp1[2].trim().split(/ +/));
+					teams.push({
+						name: team_name,
+						player_names: team_player_names
+					});
 				}
-				if (line_txts[0].length != line_txts[1].length) {
+				if (teams[0].player_names.length != teams[1].player_names.length) {
 					client.reply(data, "双方队员数量不一致。");
 					return;
-				}
-				for (var i = 0; i < 2; ++i) { 
-					const team = {
-						name: line_txts[i][0],
-						player_names: []
-					};
-					for (var j = 1; j < line_txts[i].length; ++j) { 
-						team.player_names.push(line_txts[i][j]);
-					}
-					teams.push(team);
 				}
 				client.create_kof(data, teams, time, place, is_kof);
 				break;
